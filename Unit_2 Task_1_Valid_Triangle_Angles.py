0be9bb94d1dbe32f5d3087a6635f069280d9e8c1
jashwanth466 <jaswanthnaidu466@gmail.com>
2026-09-25 14:24:10 +0530

    Add files via upload

---
+++ Unit_2 Task_1_Valid_Triangle_Angles.py
@@ -0,0 +1,10 @@
+a=float(input("Enter angle value(a):"))
+b=float(input("Enter angle value(b):"))
+c=float(input("Enter angle value(c):"))
+
+if a>0 and b>0 and c>0 and a+b+c==180:
+    print("Yes, it forms a Triangle")
+else:
+    print("No, it does not form a Triangle")
+
+
